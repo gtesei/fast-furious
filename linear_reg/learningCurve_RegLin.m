d485978d4e5464e7393f8615457013dc866d05c8
gtesei <gtesei@yahoo.com>
2014-01-12 07:19:06 +0100

    fixed buffering support

--- linear_reg/learningCurve_RegLin.m
+++ linear_reg/learningCurve_RegLin.m
@@ -3,14 +3,15 @@ function [error_train, error_val] = ...
 
   m = size(Xtrain, 1);
   b = ceil(m/500);
-  error_train = zeros(m, 1);
-  error_val   = zeros(m, 1);
+  samples = ceil(m/b);
+  error_train = zeros(samples, 1);
+  error_val   = zeros(samples, 1);
   
   [X_poly_train,mu,sigma] = treatContFeatures(Xtrain,p);
   [X_poly_val,mu_val,sigma_val] = treatContFeatures(Xval,p,1,mu,sigma);
 
   %% computing error_train, err_val
-  for i = 1:b:m
+  for i = b:b:m
   	[theta] = trainLinearReg(X_poly_train(1:i,:), ytrain(1:i,:), lambda);
   	[error_train(i), grad_train] = linearRegCostFunction(X_poly_train(1:i,:), ytrain(1:i,:), theta, 0);
   	[error_val(i), grad_cv] = linearRegCostFunction(X_poly_val,   yval,   theta, 0);
@@ -19,7 +20,7 @@ function [error_train, error_val] = ...
 
   if (length(error_train) < 100)
     fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
-    for i = 1:b:m
+    for i = b:b:m
       fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
     endfor
   endif
@@ -28,7 +29,7 @@ function [error_train, error_val] = ...
   max_X = m;
   max_Y = max(max(error_train) , max(error_val));
   min_Y = min(min(error_train) , min(error_val));
-  plot(1:b:m, error_train, 1:b:m, error_val);
+  plot(b:b:m, error_train, b:b:m, error_val);
   title(sprintf('Learning Curve (lambda = %f, p = %i)', lambda,p))
   xlabel('Train data set size')
   ylabel('Error')
