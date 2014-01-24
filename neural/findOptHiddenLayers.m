fcd40f7a2a4fe2c236018e07e6910a5a55d4006a
gtesei <gtesei@yahoo.com>
2014-01-24 19:12:32 +0100

    bugs fixes

--- neural/findOptHiddenLayers.m
+++ neural/findOptHiddenLayers.m
@@ -4,7 +4,7 @@ function [h_opt,J_opt] = findOptHiddenLayers(Xtrain, ytrain, Xval, yval , lambda
   num_label = unique(ytrain);
   s1 = n-1;
   step = 1;
-  hl = 3:step:30; 
+  hl = 1:step:5; 
   printf("|-> findOptHiddenLayers: detected %i features and %i classes ... \n",s1,length(num_label));
   printf("|-> findOptHiddenLayers: setting  %i  neurons per layers... \n",s1);
   printf("|-> findOptHiddenLayers: setting  hidden layers = %i,%i ... %i ... \n",min(hl),min(hl)+step,max(hl));
@@ -37,13 +37,13 @@ function [h_opt,J_opt] = findOptHiddenLayers(Xtrain, ytrain, Xval, yval , lambda
   fprintf('Optimal Number of hidden layers s ==  %i , Minimum Cost == %f \n', h_opt , J_opt);
 
   %%plot 
-  plot(hl, error_train, s, error_val);
+  plot(hl, error_train, hl, error_val);
   text(h_opt+1,J_opt+6,"Optimal Number of Hidden Layers","fontsize",10);
   line([h_opt,J_opt],[h_opt+1,J_opt+5],"linewidth",1);
   title(sprintf('Finding optimal number of Hidden Layers (lambda = %f , number of neurons per layer = %i)', lambda, s1));
   xlabel('Number of Neurons per Hidden Layer')
   ylabel('Error')
-  max_X = max(s);
+  max_X = max(hl);
   max_Y = max(max(error_train) , max(error_val));
   axis([0 max_X 0 max_Y]);
   legend('Train', 'Cross Validation')
