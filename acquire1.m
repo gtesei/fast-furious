95e31f49adf3b1c857c2940abd53c9d34ece1b26
unknown <gino.tesei@PC-0052.rete.brandart.it>
2014-05-23 16:32:01 +0200

    bug fix

--- acquire1.m
+++ acquire1.m
@@ -10,6 +10,9 @@ printf("|--> loading Xtrain, ytrain files ...\n");
 X = dlmread([curr_dir "/dataset/acquire-valued-shoppers-challenge/" XtrainFile]); 
 y = dlmread([curr_dir "/dataset/acquire-valued-shoppers-challenge/" ytrainFile]); 
 
+X = X(1:1000,:);
+y = y(1:1000,:);
+
 [m,n] = size(X)
 rand_indices = randperm(m);
 [Xtrain,ytrain,Xval,yval] = splitTrainValidation(X(rand_indices,:),y(rand_indices,:),0.70); 
@@ -18,5 +21,6 @@ rand_indices = randperm(m);
 printf("|--> FINDING BEST PARAMS   ...\n");
 [n_opt,J_opt] = findOptNeuronsPerLayer(Xtrain, ytrain, Xval, yval , lambda=0 ,start_neurons=-1,end_neurons=-1,step_fw=-1,hidden_layers=1,_num_label=-1, verbose=0);
 [h_opt,J_opt] = findOptHiddenLayers(Xtrain, ytrain, Xval, yval , lambda=0,neurons_hidden_layers=n_opt,_num_label=-1, verbose=0);
+num_label = 1;
 NNMeta = buildNNMeta([(n - 1) repmat(n_opt,1,h_opt) num_label]);disp(NNMeta);
 [lambda_opt,J_opt] = findOptLambda(NNMeta, Xtrain, ytrain, Xval, yval , lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]');
