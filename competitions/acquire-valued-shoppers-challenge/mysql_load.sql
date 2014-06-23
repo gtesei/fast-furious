3df4191d75bcb76da272af7281976044deec3dea
gtesei <gtesei@yahoo.com>
2014-06-23 16:59:21 +0200

    added script mysql

---
+++ competitions/acquire-valued-shoppers-challenge/mysql_load.sql
@@ -0,0 +1,30 @@
+﻿LOAD DATA LOCAL INFILE  'c:/docs/ff/gitHub/fast-furious/dataset/acquire-valued-shoppers-challenge/trainHistory'
+INTO TABLE TRAIN_HISTORY
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(id,chain,offer,market,repeattrips,repeater,offerdate);
+
+
+
+
+LOAD DATA LOCAL INFILE  'c:/docs/ff/gitHub/fast-furious/dataset/acquire-valued-shoppers-challenge/testHistory'
+INTO TABLE TEST_HISTORY
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(id,chain,offer,market,offerdate);
+
+
+LOAD DATA LOCAL INFILE  'c:/docs/ff/gitHub/fast-furious/dataset/acquire-valued-shoppers-challenge/offers'
+INTO TABLE OFFER
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(offer,category,quantity,company,offervalue,brand);
+
+
+
