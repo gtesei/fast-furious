26fefc88c0b5cfb92a3d3e60bec489ab36a27d0f
gtesei <gtesei@yahoo.com>
2014-10-10 06:30:37 +0200

    ..

--- competitions/seizure-prediction/notes_seizure_ROC.R
+++ competitions/seizure-prediction/notes_seizure_ROC.R
@@ -53,113 +53,54 @@ Xtrain_mean_sd.xval <- Xtrain_mean_sd[-forTraining,]
 Xtrain_quant.train <- Xtrain_quant[ forTraining,]
 Xtrain_quant.xval <- Xtrain_quant[-forTraining,]
 
-ytrain.cat = as.factor(ytrain[forTraining,2]);
-ytest.cat = as.factor(ytrain[-forTraining,2]);
+ytrain.cat = as.factor(ytrain[forTraining,2])
+ytest.cat = as.factor(ytrain[-forTraining,2])
+
+levels(ytrain.cat) = levels(ytest.cat) = c("inter-ict","pre-ict")
 
 verbose = T
 controlObject <- trainControl(method = "repeatedcv", repeats = 5, number = 10)
 
-######################################################## linear regression 
-# if (verbose) cat("****** [Xtrain_mean_sd] linear regression ...  \n")
-# set.seed(669); ptm <- proc.time()
-# linearReg <- train(time_before_seizure ~  . , data = Xtrain_mean_sd.train, method = "lm", trControl = controlObject) 
-# if (verbose) linearReg
-# tm = proc.time() - ptm
-# perf.grid = predictAndMeasure (linearReg,"Linear Reg (mean sd)",Xtrain_mean_sd.train,
-#                                Xtrain_mean_sd.train$time_before_seizure,
-#                                Xtrain_mean_sd.xval,Xtrain_mean_sd.xval$time_before_seizure,
-#                                tm ,  ytrain.cat , ytest.cat ,    
-#                                grid = NULL , verbose)
-
+######
 if (verbose) cat("****** [Xtrain_quant] linear regression ...  \n")
 set.seed(669); ptm <- proc.time()
 linearReg.quant <- train(time_before_seizure ~  . , data = Xtrain_quant.train, method = "lm", trControl = controlObject) 
 if (verbose) linearReg.quant
 tm = proc.time() - ptm
-# perf.grid = predictAndMeasure (linearReg.quant,"Linear Reg (quantiles)",Xtrain_quant.train,
-#                                Xtrain_quant.train$time_before_seizure,
-#                                Xtrain_quant.xval,Xtrain_quant.xval$time_before_seizure, 
-#                                tm,  ytrain.cat , ytest.cat ,    
-#                                grid = perf.grid , verbose )
 
-##########
-set.seed(669); ptm <- proc.time()
-plsModel <- train(time_before_seizure ~  . , data = Xtrain_mean_sd.train , method = "pls", preProc = c("center", "scale"), 
-                  tuneLength = 15, trControl = controlObject)
-#if (verbose) plsModel
-tm = proc.time() - ptm
-##########
 
-model = plsModel
+model = linearReg.quant
 trainingData = Xtrain_quant.train
 ytrain = Xtrain_quant.train$time_before_seizure
 testData = Xtrain_quant.xval
 ytest = Xtrain_quant.xval$time_before_seizure
-model.label = "Linear Reg (mean sd)"
+model.label = "Linear Reg (quant)"
 grid = NULL
 
 
 pred.train = predict(model , trainingData) 
 RMSE.train = RMSE(obs = ytrain , pred = pred.train)
-
 pred = predict(model , testData) 
 RMSE.test = RMSE(obs = ytest , pred = pred)
-
 if (verbose) cat("****** RMSE(train) =",RMSE.train," -  RMSE(test) =",RMSE.test,"  --  Time elapsed(sec.):",tm[[3]], "...  \n")
 
 ## fitter.cat 
 train.cat = data.frame( cat = ytrain.cat , pr =  pred.train )
-#test.cat = data.frame(cat = ytest.cat , pr =  pred )
 test.cat = data.frame( pr =  pred )
-ctrl <- trainControl(summaryFunction = twoClassSummary, classProbs = TRUE)
-#fitter.cat <- train( cat ~ pr ,  data=train.cat , method = "glm", metric = "ROC", trControl = ctrl)
+
+## glm 
 fitter.cat <- glm( cat ~ pr ,  data=train.cat , family = binomial)
-pred.train.cat = predict(fitter.cat, newdata = train.cat , type = "response") 
-pred.cat = predict(fitter.cat, newdata = test.cat , type = "response") 
+pred.train.glm = predict(fitter.cat, newdata = train.cat , type = "response") 
+pred.test.glm = predict(fitter.cat, newdata = test.cat , type = "response") 
 
-acc.train = sum(    factor(ifelse(pred.train.cat > 0.5,1,0), levels=levels(ytrain.cat)) ==   ytrain.cat   ) / length(ytrain.cat)
-acc.test = sum(     factor(ifelse(pred.cat > 0.5,1,0),       levels=levels(ytrain.cat)) ==   ytest.cat  ) / length(ytest.cat)
+acc.train = sum(    factor(ifelse(pred.train.glm > 0.5,1,0), levels=levels(ytrain.cat)) ==   ytrain.cat   ) / length(ytrain.cat)
+acc.test = sum(     factor(ifelse(pred.test.glm > 0.5,1,0),       levels=levels(ytrain.cat)) ==   ytest.cat  ) / length(ytest.cat)
 
-#roc.train = roc.area(as.numeric(ytrain.cat == 1) , as.numeric(pred.train.cat == 1) )$A
-#roc.test = roc.area(as.numeric(ytest.cat == 1) , as.numeric(pred.cat == 1) )$A
-
-rocCurve <- roc(response = ytrain.cat, predictor = as.numeric(pred.train.cat), levels = rev(levels(ytrain.cat)))
+rocCurve <- roc(response = ytrain.cat, predictor = as.numeric(pred.train.glm), levels = rev(levels(ytrain.cat)))
 roc.train = as.numeric( auc(rocCurve) )
 
-rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.cat ), levels = rev(levels(ytest.cat)))
+rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.test.glm ), levels = rev(levels(ytest.cat)))
 roc.test = as.numeric( auc(rocCurve) )
-
-### perf.grid 
-perf.grid = NULL
-if (is.null(grid)) { 
-  perf.grid = data.frame(predictor = c(model.label) , RMSE.train = c(RMSE.train) , RMSE.test = c(RMSE.test) , 
-                         acc.train = c(acc.train) , acc.test = c(acc.test) , 
-                         roc.train = c(roc.train) , roc.test =c(roc.test),
-                         time = c(tm[[3]]))
-} else {
-  .grid = data.frame(predictor = c(model.label) , RMSE.train = c(RMSE.train) , 
-                     RMSE.test = c(RMSE.test) , 
-                     acc.train = c(acc.train) , acc.test = c(acc.test) , 
-                     roc.train = c(roc.train) , roc.test =c(roc.test),
-                     time = c(tm[[3]]) )
-  perf.grid = rbind(grid, .grid)
-}
-
-perf.grid
-
-####### proviamo l'errore 
-rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.cat > 0.5), levels = rev(levels(ytest.cat)))
-roc.test = as.numeric( auc(rocCurve) )
-roc.test ## 0.3375 !!!!! 
-ci.roc(rocCurve)
-plot(rocCurve, legacy.axes = TRUE)
-
-rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.cat ), levels = rev(levels(ytest.cat)))
-roc.test = as.numeric( auc(rocCurve) )
-roc.test ## 0.7194444 !! 
-ci.roc(rocCurve)
-plot(rocCurve, legacy.axes = TRUE)
-plot(pred.cat,ytest.cat) 
 
 
 ###########
@@ -168,45 +109,51 @@ rfModel <- randomForest(cat ~ pr ,  data=train.cat , ntree = 2000)
 library(MASS) ## for the qda() function
 qdaModel <- qda(cat ~ pr ,  data=train.cat)
 
-qdaTrainPred <- predict(qdaModel, train.cat)
-names(qdaTrainPred)
-head(qdaTrainPred$class)
-head(qdaTrainPred$posterior)
+pred.train.qda <- predict(qdaModel, train.cat)
+names(pred.train.qda)
+head(pred.train.qda$class)
+head(pred.train.qda$posterior)
 
-qdaTestPred <- predict(qdaModel, test.cat)
-pred.train.cat  <- qdaTrainPred$posterior[,"1"]
-pred.cat <- qdaTestPred$posterior[,"1"]
-rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.cat ), levels = rev(levels(ytest.cat)))
+pred.test.qda <- predict(qdaModel, test.cat)
+pred.test.qda.pre  <- pred.test.qda$posterior[,"pre-ict"]
+rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.test.qda.pre ), levels = rev(levels(ytest.cat)))
 roc.test = as.numeric( auc(rocCurve) )
 roc.test ## 0.7194444 !! 
 ci.roc(rocCurve)
 plot(rocCurve, legacy.axes = TRUE)
 
 
-rfTestPred <- predict(rfModel, test.cat, type = "prob")
-head(rfTestPred)
-pred.cat <- rfTestPred[,"1"]
-rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.cat ), levels = rev(levels(ytest.cat)))
+
+######### rf
+pred.test.rf <- predict(rfModel, test.cat, type = "prob")
+head(pred.test.rf)
+pred.test.rf.pre <- pred.test.rf[,"pre-ict"]
+rocCurve <- roc(response = ytest.cat, predictor = as.numeric(pred.test.rf.pre ), levels = rev(levels(ytest.cat)))
 roc.test = as.numeric( auc(rocCurve) )
 roc.test ## 0.7118056 !! 
 ci.roc(rocCurve)
 plot(rocCurve, legacy.axes = TRUE)
 
 
-ctrl <- trainControl(summaryFunction = twoClassSummary, classProbs = TRUE)
-set.seed(202)
-sigmaRangeReduced <- sigest(as.matrix( pred.train ))
-svmRGridReduced <- expand.grid(.sigma = sigmaRangeReduced[1],
-                                  .C = 2^(seq(-4, 4)))
-set.seed(476)
-svmRModel <- train(cat ~ pr ,  data=train.cat , 
-                     method = "svmRadial",
-                     metric = "ROC",
-                     preProc = c("center", "scale"),
-                     tuneGrid = svmRGridReduced,
-                     fit = FALSE,
-                     trControl = ctrl)
+#### calibration 
+calCurve <- calibration(ytest.cat ~ pred.test.qda.pre + pred.test.rf.pre )
+calCurve
+xyplot(calCurve, auto.key = list(columns = 2))
 
-pred.cat = predict(svmRModel, train.cat , type = "prob" )
+sigmoidalCal <- glm(relevel(ytest.cat, ref = "inter-ict") ~ pred.test.qda.pre , family = binomial)
+coef(summary(sigmoidalCal)) 
+sigmoidProbs <- predict(sigmoidalCal, newdata = pred.test.qda.pre, type = "response")
+calCurve <- calibration(ytest.cat ~ pred.test.qda.pre + pred.test.rf.pre + sigmoidProbs)
+calCurve
+xyplot(calCurve, auto.key = list(columns = 2))
 
-svmRModel
+
+library(klaR)
+dd = data.frame(class = ytest.cat , pr = pred.test.qda.pre)
+BayesCal <- NaiveBayes(class ~ pr , data = dd,  usekernel = TRUE)
+BayesProbs <- predict(BayesCal, newdata = dd[, "pr", drop = FALSE])
+QDABayes <- BayesProbs$posterior[, "pre-ict"]
+calCurve2 <- calibration(ytest.cat ~ pred.test.qda.pre + pred.test.rf.pre + QDABayes)
+xyplot(calCurve2)
+xyplot(calCurve2, auto.key = list(columns = 2))
+
