0dbdd420cb008f9fecd9a12db32846dfa3ba8c2c
gtesei <gtesei@yahoo.com>
2014-10-22 17:34:36 +0200

    ..

---
+++ util/statisticalMomentPowerSpectrum.m
@@ -0,0 +1,22 @@
+function [p1,p2,p3,p4,p5,P] = statisticalMomentPowerSpectrum (fs,T,x) 
+  % fs = Sample frequency (Hz)
+  % T = secs sample
+  % x = signa
+  
+  t = 0:1/fs:T-1/fs;      
+  n = length(t);
+  
+  swaveX = fft(x)/ n;
+  hz = linspace( 0, fs/2, floor(n/2) + 1);
+  
+  nyquistfreq = fs/2;
+
+  hzIdxTail = dsearchn( hz', nyquistfreq);
+  P =  sum(swaveX.*conj(swaveX));
+  
+  m1 = (1/P)*2*sum(swaveX(1:hzIdxTail).*conj(swaveX(1:hzIdxTail)).* (1:hzIdxTail));
+  m2 = (1/P)*2*sum(swaveX(1:hzIdxTail).*conj(swaveX(1:hzIdxTail)).* ((1:hzIdxTail).*(1:hzIdxTail)) );
+  m2 = (1/P)*2*sum(swaveX(1:hzIdxTail).*conj(swaveX(1:hzIdxTail)).* (1:hzIdxTail).*(1:hzIdxTail).*(1:hzIdxTail)  );
+  m4 = (1/P)*2*sum(swaveX(1:hzIdxTail).*conj(swaveX(1:hzIdxTail)).* (1:hzIdxTail).*(1:hzIdxTail).*(1:hzIdxTail).*(1:hzIdxTail)  );
+  
+endfunction
