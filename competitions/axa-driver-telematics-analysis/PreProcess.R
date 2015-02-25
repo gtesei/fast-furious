71497e2e3dbd0c00ac8e1bec543b02757ec3e224
gtesei <gtesei@yahoo.com>
2015-02-25 18:18:26 +0100

    ale

--- competitions/axa-driver-telematics-analysis/PreProcess.R
+++ competitions/axa-driver-telematics-analysis/PreProcess.R
@@ -7,6 +7,7 @@ library(pROC)
 library(kernlab)
 library(subselect)
 library(plyr)
+library(binhf)
 
 getTripPath = function (driver = 0 , trip = 0) {
   ret = ""
@@ -28,39 +29,85 @@ getTripPath = function (driver = 0 , trip = 0) {
   ret
 } 
 
-
-
-
-
-
+######################### settings ans constants 
+debug = T
 
 ######################### main loop 
 
 DRIVERS = c(1,2)
 TRIPS = c(1,2)
 
-driver = 1
+drv = 1
 trip = 1
 
-while (! is.na(getTripPath(driver,trip))) {
-  trip = 1
-  if ( ! (exists("DRIVERS") & ! is.element(driver,DRIVERS))  ) {
+while (! is.na(getTripPath(drv,trip))) {
+  
+  if ( ! (exists("DRIVERS") & ! is.element(drv,DRIVERS))  ) {
      
-    while (! is.na(getTripPath(driver,trip))) {
+    while (! is.na(getTripPath(drv,trip))) {
       if ( ! (exists("TRIPS") & ! is.element(trip,TRIPS))  ) {
         
-        cat("|---------------->>> processing driver: <<",driver,">>  <<",trip,">> ..\n")
+        ############ TRIP PROCESSING - begin 
+        cat("|---------------->>> processing driver: <<",drv,">>  <<",trip,">> ..\n")
+        
+        #### load data 
+        trdata = as.data.frame(fread(getTripPath(drv,trip)))
+        if (debug) print(head(trdata))
+        
+        #### extract 1-tier features  
+        
+        ## rho 
+        trdata$rho = apply(trdata,1,function(x) {
+              sqrt(x[1]^2 + x[2]^2)
+          	})
+        
+        ## alpha 
+        trdata$alpha = apply(trdata,1,function(x) {
+          ret = 0 
+          if (x[1] == 0) ret = 0
+          else ret = atan(x[2]/x[1])
+          ret
+        })
+        
+        ## vx 
+        trdata$vx =  trdata$x - shift(trdata$x,1)
+        trdata$vx[1] = 0
+        
+        ## vy
+        trdata$vy =  trdata$y - shift(trdata$y,1)
+        trdata$vy[1] = 0
+        
+        ## V 
+        trdata$V =  apply(trdata,1,function(x) {
+          sqrt(x[5]^2 + x[6]^2)
+        })
+        
+        ## ax 
+        trdata$ax =  trdata$vx - shift(trdata$vx,1)
+        trdata$ax[1] = 0
+        
+        ## ay
+        trdata$ay =  trdata$vy - shift(trdata$vy,1)
+        trdata$ay[1] = 0
+        
+        ## A 
+        trdata$A =  apply(trdata,1,function(x) {
+          sqrt(x[8]^2 + x[9]^2)
+        })
+        #### extract 2-tier features  
+        
+        #### update featutures set   
+        
+        if (debug) print(head(trdata))
+        ############ TRIP PROCESSING - end 
       }
       
       trip = trip + 1 
     } 
   }
   
-  driver = driver + 1 
-  
-  
-  
-  
+  drv = drv + 1 
+  trip = 1
 } 
 
 
