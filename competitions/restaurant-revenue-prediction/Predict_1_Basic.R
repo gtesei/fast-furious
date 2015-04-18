5413c207d5d2cb60e5fb4b524c4d50184a871d7a
gtesei <gtesei@yahoo.com>
2015-04-18 13:51:57 +0200

    refactoring categorical features encoding

--- competitions/restaurant-revenue-prediction/Predict_1_Basic.R
+++ competitions/restaurant-revenue-prediction/Predict_1_Basic.R
@@ -86,47 +86,12 @@ buildData.basic = function(train.raw , test.raw) {
   list(train,y,test)
 }
 
-
-encodeCategoricalFeature = function(data.train , data.test , colname.prefix, asNumeric=T) {
-  ### assembling 
-  data = c(data.test , data.train)
-  
-  ###
-  fact_min = 1 
-  fact_max = -1
-  facts = NULL
-  if (asNumeric) {
-    fact_max = max(unique(data))
-    fact_min = min(unique(data))
-    facts = fact_min:fact_max
-  } else {
-    facts = sort(unique(data))
-  }
-  
-  ##
-  mm = matrix(rep(0,length(data)),nrow=length(data),ncol=length(facts))
-  colnames(mm) = paste(paste(colname.prefix,"_",sep=''),    facts   ,sep='')
-  
-  ##
-  for ( j in 1:length(facts) ) 
-    for (i in 1:length(data))
-      mm[i,j] = (data[i] == facts[j])
-  
-  ##
-  mm = as.data.frame(mm)
-  
-  ## reassembling 
-  testdata = mm[1:(length(data.test)),]
-  traindata = mm[((length(data.test))+1):(dim(mm)[1]),]
-  
-  list(traindata,testdata)
-}
-
 #######
 verbose = T
 source(paste0( getBasePath("process") , "/FeatureSelection_Lib.R"))
 source(paste0( getBasePath("process") , "/Resample_Lib.R"))
 source(paste0( getBasePath("process") , "/Regression_Lib.R"))
+source(paste0( getBasePath("process") , "/FeatureEncode_Lib.R"))
 
 RegModels = c("Average" , "Mode",  
               "LinearReg", "RobustLinearReg", 
