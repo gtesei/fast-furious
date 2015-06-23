e3d9a4d2f610b5067a7475d5fae416436aeda54f
gtesei <gtesei@yahoo.com>
2015-06-23 16:42:46 -0700

    matlab starter neural network / regularized polynomial (logistic) regression

---
+++ GO.m
@@ -0,0 +1,18 @@
+#! /opt/local/bin/octave -qf 
+
+##setting enviroment 
+menv
+
+##Test Cases
+#source "./linear_reg/README.m";
+#go();
+
+%source "./logistic_reg/README.m";
+%go();
+
+source "./neural/README.m";
+go();
+
+
+
+
