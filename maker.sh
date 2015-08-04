d98297eabeed58f53777dd8b2150ac727b60159a
gtesei <gtesei@yahoo.com>
2015-08-04 15:51:27 -0700

    make r

---
+++ maker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+read -p "Commit description: " desc
+
+echo ">> updating manuals ... "
+Rscript -e 'library(roxygen2);roxygen2::roxygenise(package.dir = "R-package")'
+
+if [[ $? -eq 0 ]]; then 
+      echo ">> checking package ..."
+      #R CMD check R-package --as-cran
+      R CMD check R-package
+      if [[ $? -eq 0 ]]; then
+            echo ">> adding updates ..."
+            git status 
+            git add R-package/*
+            if [[ $? -eq 0 ]]; then
+                  echo ">> committing and pushing to github ..."
+                  git status 
+                  git commit -m "$desc"
+                  git push origin master 
+            fi    
+      fi 
+fi 
