a5f468bd5abc03653776ab3746f76bc0422801ec
gtesei <gtesei@yahoo.com>
2015-08-11 11:09:52 -0700

    --as-cran

--- maker.sh
+++ maker.sh
@@ -7,8 +7,8 @@ Rscript -e 'library(roxygen2);roxygen2::roxygenise(package.dir = "R-package")'
 
 if [[ $? -eq 0 ]]; then 
       echo ">> checking package ..."
-      #R CMD check R-package --as-cran
-      R CMD check R-package
+      R CMD check R-package --as-cran
+      #R CMD check R-package
       if [[ $? -eq 0 ]]; then
             echo ">> adding updates ..."
             git status 
