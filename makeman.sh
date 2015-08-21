ea8ac57ed3925f5857a4e826c16fb2b3c3b15aaf
gtesei <gtesei@yahoo.com>
2015-08-21 01:24:05 -0700

    make manual

--- makeman.sh
+++ makeman.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo ">> updating manuals ... "
+git pull origin master 
 Rscript -e 'library(roxygen2);roxygen2::roxygenise(package.dir = "R-package")'
-
 if [[ $? -eq 0 ]]; then 
       echo ">> checking package ..."
       R CMD check R-package --as-cran
