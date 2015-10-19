f3b7e30b2db9ea8a6367b3fe720fa01fb9af1f34
gtesei <gtesei@yahoo.com>
2015-10-19 15:47:28 -0700

    kmeans

--- R-package/R/fastUnsupervised.R
+++ R-package/R/fastUnsupervised.R
@@ -121,3 +121,91 @@ ff.pca <- function(Xtrain,Xtest,
               PC.train = PC.train ,  PC.test = PC.test ))
 }
 
+#' An useful wrapper of \code{\link[stats]{kmeans}} performing k-means clustering on the given trainset / testset (\code{Xtrain} / \code{Xtest}) and 
+#' assuming a number of cluster from 1:\code{max_centers}. The best number of cluster is computed so that the variation in 
+#' the within group sum of squares between two subsequent number of clusters is maximized in absolute value.  
+#' 
+#' @param Xtrain the encoded \code{data.frame} of train data. Must be a \code{data.frame} of \code{numeric}
+#' @param Xtest the encoded \code{data.frame} of train data. Must be a \code{data.frame} of \code{numeric}
+#' @param max_centers the max number of clusters to be evaluated 
+#' @param nstart how many random sets should be chosen? Such a parameter is passed to \code{\link[stats]{kmeans}}. 
+#' @param iter.max the maximum number of iterations allowed. Such a parameter is passed to \code{\link[stats]{kmeans}}. 
+#' @param varThreshold a threshold indicating the proportion of variance that should be explained. Must be a numeric between 0 and 1. 
+#' @param doPlot a logical value indicating whether plotting.  
+#' @param verbose a logical value indicating whether verbose mode should be enabled.  
+#' @examples
+#' ## data 
+#' Xtrain <- data.frame(a = rep(1:10 , each = 2), b = 20:1, 
+#'                      c = rep(as.Date(c("2007-06-22", "2004-02-13")),10) , d = 20:1)
+#' Xtest <- data.frame(a = rep(2:11 , each = 2), b = 1:20, 
+#'                     c = rep(as.Date(c("2007-03-01", "2004-05-23")),10) , d = 1:20)
+#' 
+#' ## encode data sets 
+#' l = ff.makeFeatureSet(Xtrain,Xtest,c("C","N","D","N"))
+#' Xtrain = l$traindata
+#' Xtest = l$testdata
+#' 
+#' ffKmeans = ff.kmeans(Xtrain = Xtrain , Xtest = Xtest , 
+#'                      max_centers = 10 , verbose = TRUE)
+#' 
+#' best_n_cluters = ffKmeans$best_n_cluters 
+#' 
+#' K.train = ffKmeans$K.train
+#' K.test = ffKmeans$K.test
+#' 
+#' @export
+#' @return a list whose components are the max number of clusters evaluated (\code{max_centers}), the best number of clusters
+#'  (\code{best_n_cluters}), assuming \code{best_n_cluters} as number of clusters a vector of integers (from 1:\code{best_n_cluters}) 
+#'  indicating the cluster to which each point is allocated in the train set (\code{K.train}) and test set (\code{K.test}).       
+#' 
+ff.kmeans <- function(Xtrain,Xtest,
+                     max_centers = 10, nstart = 5, iter.max = 10, 
+                     doPlot=FALSE,verbose=FALSE) {
+  if (!is.null(Xtrain) & !is.null(Xtest)) stopifnot(  sum(colnames(Xtrain) != colnames(Xtest)) == 0 )
+  stopifnot( ! is.null(Xtrain) || ! is.null(Xtest) )
+  
+  is_Xtrain_NULL <- is.null(Xtrain)
+  is_Xtest_NULL <- is.null(Xtest)
+  n.train <- if (is_Xtrain_NULL) NA else nrow(Xtrain)
+  
+  data = rbind(Xtrain,Xtest)
+  rm(list=c("Xtrain","Xtest"))
+  
+  wss = rep(NA,max_centers)
+  for (i in 1:max_centers) wss[i] <- sum(kmeans(x = data , iter.max = iter.max , centers = i , nstart = nstart)$withinss)
+  
+  ## plot
+  if (doPlot) {
+    plot(1:max_centers, wss, type="b", 
+         xlab="Number of Clusters",
+         ylab="Within groups sum of squares", 
+         main="Within groups sum of squares vs. Number of Clusters")  
+  }
+  
+  ## find best number of clusters 
+  wss_delta = rep(0,length(wss)-1)
+  for (i in 1:length(wss)-1) wss_delta[i] = (wss[i+1] - wss[i])/wss[i]
+  best_n_cluters = which(wss_delta == min(wss_delta))+1
+  if (verbose) cat(">>> best number of clusters:",best_n_cluters," ...\n")
+  
+  ## kopt 
+  kopt = kmeans(x = data , centers = best_n_cluters , nstart = nstart)
+  
+  ## PC.train / PC.test 
+  if (!is_Xtrain_NULL && !is_Xtest_NULL) {
+    K.train = kopt$cluster[1:n.train]
+    K.test = kopt$cluster[((n.train+1):length(kopt$cluster))]  
+  } else if (!is_Xtrain_NULL ) {
+    K.train = kopt$cluster
+    K.test = NULL  
+  } else {
+    K.train = NULL
+    K.test = kopt$cluster
+  }
+  
+  return(list(max_centers = max_centers,
+              best_n_cluters = best_n_cluters,
+              K.train = K.train , 
+              K.test = K.test ))
+}
+
--- R-package/tests/testthat/test-fastUnsupervised.R
+++ R-package/tests/testthat/test-fastUnsupervised.R
@@ -25,3 +25,33 @@ test_that('pca', {
   options(warn=warn_def)
   
 })
+
+test_that('k-means', {
+  #skip_on_cran()
+  
+  ## suppress warnings raised because there few obs 
+  warn_def = getOption('warn')
+  options(warn=-1)
+  
+  ## data 
+  Xtrain <- data.frame( a = rep(1:10 , each = 2), b = 20:1, c = rep(as.Date(c("2007-06-22", "2004-02-13")),10) , d = 20:1)
+  Xtest <- data.frame( a = rep(2:11 , each = 2), b = 1:20, c = rep(as.Date(c("2007-03-01", "2004-05-23")),10) , d = 1:20)
+  
+  ## encode data sets 
+  l = ff.makeFeatureSet(Xtrain,Xtest,c("C","N","D","N"))
+  Xtrain = l$traindata
+  Xtest = l$testdata
+  
+  MAX_CENTERS = 10 
+  
+  ffKmeans = ff.kmeans(Xtrain = Xtrain , Xtest = Xtest , max_centers = MAX_CENTERS , nstart = 5 , verbose = TRUE)
+  
+  expect_lt(object = ffKmeans$best_n_cluters , expected = ffKmeans$max_centers)
+  expect_equal(object = length(ffKmeans$K.train),expected = nrow(Xtrain))
+  expect_equal(object = length(ffKmeans$K.test),expected = nrow(Xtest))
+  expect_equal(object = ffKmeans$max_centers ,expected = MAX_CENTERS)
+  
+  ## restore warnings 
+  options(warn=warn_def)
+  
+})
